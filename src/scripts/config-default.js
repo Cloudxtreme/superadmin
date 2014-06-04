262438d36d890c2b47f70768fb3e89ad8ab1ce8b
Daniel STANCU <daniel@bmgroup.be>
2014-06-04 08:43:38 +0000

    Default configuration file

--- src/scripts/config-default.js
+++ src/scripts/config-default.js
@@ -2,16 +2,12 @@ define({
     environment: 'development',
 
     // API settings
-    app: {
-        tokenName: 'access_token',
-        sessionName: 'session'
-    },
     api: {
         url: 'http://cloudwalkers-website.local/',
         path : 'json/admin',
         authPath: 'oauth2/authorize',
         redirectUrl: 'http://cloudwalkers-superadmin.local/oauth.html',
-        clientId: '680f7d83014fa3aca5ca054e3ebda5f70538ed73d',
+        clientId: '',
         state: 'xyz',
         scope: ''
     }
