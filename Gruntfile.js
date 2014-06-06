0a6de927414ceb12c26f25ec9f4e698034c93c80
Daniel STANCU <daniel@bmgroup.be>
2014-06-06 08:43:56 +0000

    Adding Grunt tasks release/debug

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // Project configuration.
     grunt.initConfig({
